bd6aa598e9c4eabea4c7065f7b7d2ffe9f20a8e1
oztasyasin <oztas.yasin@hotmail.com>
2024-09-28 02:15:04 +0300

    refactor - Api file type changed

---
+++ src/requests/Api.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
+export const APP_ID = process.env.EXPO_PUBLIC_APPLICATION_ID;
+export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+export const RESOURCE_VERSION = process.env.EXPO_PUBLIC_RESOURCE_VERSION;
+const Api = axios.create({
+	baseURL: API_URL,
+	timeout: 10000,
+	headers: {
+		'Content-Type': 'application/json',
+		'app_id': APP_ID,
+		'app_key': API_KEY,
+		"ResourceVersion": RESOURCE_VERSION
+	},
+});
+
+Api.interceptors.request.use(async (config) => {
+	const apiConfig = config;
+	return apiConfig;
+});
+
+
+Api.interceptors.response.use(
+	(response: AxiosResponse) => {
+		return response;
+	},
+	(error: AxiosError) => {
+		return Promise.reject(error);
+	}
+);
+
+export default Api;
