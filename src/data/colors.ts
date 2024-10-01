bca34fb69c92ada7381741cf3d6c3e4d2b682868
oztasyasin <oztas.yasin@hotmail.com>
2024-10-01 06:03:33 +0300

    add - new colors added

---
+++ src/data/colors.ts
@@ -0,0 +1,8 @@
+export const themeColors = {
+    primariy0: "#FFFFFF",
+    primary500: "#1EA1FF",
+    subText: "#4C606F",
+    primaryText: "#1A2228",
+    blue: "#1EA1FF",
+    containerColor: "#F5F9FA",
+}
