027c21c6802629a8eced13d26529c08a8ac4c6d3
oztasyasin <oztas.yasin@hotmail.com>
2024-10-01 06:01:48 +0300

    create - global styles created

---
+++ src/styles/global.ts
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    txt40_600_60_12: {
+        fontSize: 40,
+        fontWeight: '600',
+        lineHeight: 60,
+        letterSpacing: -1.2,
+        fontFamily: "jakarta_600"
+    },
+    txt16_400_26_016: {
+        fontSize: 16,
+        fontWeight: '400',
+        lineHeight: 26,
+        letterSpacing: -0.16,
+        fontFamily: "urbanist_400"
+    },
+    txt16_600_24_032: {
+        fontSize: 16,
+        fontWeight: '600',
+        lineHeight: 24,
+        letterSpacing: -0.32,
+        fontFamily: "jakarta_400"
+    },
+    txt14_500_21_028: {
+        fontSize: 14,
+        fontWeight: '500',
+        lineHeight: 21,
+        letterSpacing: -0.28,
+        fontFamily: "jakarta_400"
+    },
+    txt15_400_normal_045: {
+        fontSize: 15,
+        fontFamily: 'jakarta_400',
+        letterSpacing: -0.45
+    },
+    txt19_700_normal_057: {
+        fontSize: 19,
+        fontFamily: 'jakarta_700',
+        letterSpacing: -0.57
+    },
+    txt11_400_normal_012: {
+        fontSize: 11,
+        fontFamily: "jakarta_400",
+        letterSpacing: -0.12
+    },
+    txt13_600_normal_018: {
+        fontSize: 13,
+        fontFamily: "jakarta_600",
+        letterSpacing: -0.18
+    }
+})
