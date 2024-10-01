36539a11926dd7928f35124e2da8b481987a67e7
oztasyasin <oztas.yasin@hotmail.com>
2024-10-01 06:04:27 +0300

    refactor - flights fecther hooks optimized

--- src/hooks/flights.ts
+++ src/hooks/flights.ts
@@ -1,17 +1,53 @@
-import { useQuery } from "react-query";
 import { GetFlights } from "../requests/flights";
 import { useFlightStore } from "../store/flights";
 import { AxiosResponse } from "axios";
-
-export const useGetFlights = () => {
-    const { flights, updateFlights } = useFlightStore();
-    const handleSuccess = (res: AxiosResponse) => updateFlights(res.data?.body?.flights);
+import { useIsFocused } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { FlightHooksProps } from "../models";
+import { renderToast } from "../helper";
+export const useGetFlights = (params: FlightHooksProps) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const isFocused = useIsFocused();
+    const handleSuccess = (res: AxiosResponse) => params.updateFunction(res.data?.flights);
     const handleError = () => {
-
+        renderToast({ type: 'error', text2: "There was a problem while fetching the data" });
     }
-    const { refetch, isLoading } = useQuery(['getFlights'], GetFlights, {
-        onSuccess: handleSuccess,
-        onError: handleError,
+    const fetchData = async () => {
+        try {
+            setIsLoading(() => (true))
+            const res = await GetFlights(params.fetchParams)
+            handleSuccess(res);
+        } catch (error) {
+            handleError();
+        } finally {
+            setIsLoading(() => (false))
+        }
+    }
+    useEffect(() => {
+        isFocused && fetchData();
+    }, [isFocused])
+
+    return { isLoading, refetch: fetchData }
+}
+
+export const useGetArivalFlights = () => {
+
+    const { arivalFlights, updateArivalFlights } = useFlightStore();
+    const { isLoading, refetch } = useGetFlights({
+        updateFunction: updateArivalFlights,
+        fetchParams: { flightDirection: "A" }
     });
-    return { flights, isLoading, refetch }
+
+    return { arivalFlights, isLoading, refetch }
+}
+
+export const useGetDepartureFlights = () => {
+
+    const { departureFlights, updateDepartureFlights } = useFlightStore();
+    const { isLoading, refetch } = useGetFlights({
+        updateFunction: updateDepartureFlights,
+        fetchParams: { flightDirection: "D" }
+    });
+
+    return { departureFlights, isLoading, refetch }
 }
