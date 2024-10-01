86b62790c1bcc633a1fab5c00e6673b2e954fdcc
oztasyasin <oztas.yasin@hotmail.com>
2024-10-01 06:02:50 +0300

    refactor - flight sotre optimized for new features

--- src/store/flights.ts
+++ src/store/flights.ts
@@ -1,13 +1,32 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
+import { BookedFlight, Destination, Flight, FlightStore } from '../models';
 
 export const useFlightStore = create<FlightStore>()(
     persist(
         (set) => ({
             flights: [],
             bookedFlights: [],
+            arivalFlights: [],
+            destinations: [],
+            departureFlights: [],
             updateFlights: (data: Flight[]) => set({ flights: data }),
+            updateArivalFlights: (data: Flight[]) => set({ arivalFlights: data }),
+            updateDepartureFlights: (data: Flight[]) => set({ departureFlights: data }),
+            updateBookedFlights: (data: BookedFlight[]) => set({ bookedFlights: data }),
+            reset: () => set({
+                flights: [],
+                bookedFlights: [],
+                arivalFlights: [],
+                destinations: [],
+                departureFlights: [],
+            }),
+
+            addNewDestination: (data: Destination) =>
+                set((state: FlightStore) => ({
+                    destinations: [...state.destinations, data],
+                })),
 
             addNewBookedFlight: (data: BookedFlight) =>
                 set((state: FlightStore) => ({
